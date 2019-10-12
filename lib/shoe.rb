16d79b563bab6ca83f93294413434c8e57acdbee
Jazmin M <jazminmerckel@gmail.com>
2019-10-12 00:32:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(single_brand)
-    @brand = single_brand
-    BRANDS << single_brand 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
   end
   
 end
