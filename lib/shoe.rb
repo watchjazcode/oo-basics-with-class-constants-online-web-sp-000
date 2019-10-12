59e5ec2055cdfdc2a339fe2814eb152ea78e834e
Jazmin M <jazminmerckel@gmail.com>
2019-10-12 00:43:55 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include? brand
+      BRANDS << brand 
+    end
   end
 
   def cobble
@@ -11,4 +16,11 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(single_brand)
+    @brand = single_brand
+    if !BRANDS.include? single_brand #checking to see if a unique brand is included
+      BRANDS << single_brand 
+    end
+  end
+  
 end
